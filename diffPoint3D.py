9cc86ffc6b81d89c66ed509d95c44c8a052964b1
lyj <1334738575@qq.com>
2025-05-27 17:53:38 +0800

    add P3D2P3D

--- diffPoint3D.py
+++ diffPoint3D.py
@@ -2,7 +2,7 @@ from sympy import *
 import sympy
 from sympy.utilities.codegen import codegen
 
-outputName = "_r_diffLine3D"
+outputName = "_r_diffPoint3D"
 
 r1, p1, w1, x1, y1, z1, r2, p2, w2, x2, y2, z2 = symbols('rA pA wA xA yA zA rB pB wB xB yB zB')
 tw1 = symbols('twA:3')
@@ -11,10 +11,6 @@ tw2 = symbols('twB:3')
 Rw2 = symbols('RwB:3(:3)')
 Pc1 = symbols("PcA:3")
 Pc2 = symbols("PcB:3")
-nc1 = symbols("ncA:3")
-nc2 = symbols("ncB:3")
-dc1 = symbols("dcA:3")
-dc2 = symbols("dcB:3")
 center1 = symbols("centerA:3")
 center2 = symbols("centerB:3")
 
@@ -57,57 +53,34 @@ dT2 = Rt2T(dR2, dt2)
 
 Pc1Matrix = Matrix([Pc1[0], Pc1[1], Pc1[2], 1])
 Pc2Matrix = Matrix([Pc2[0], Pc2[1], Pc2[2], 1])
-Pc1nc1 = Matrix([nc1[0], nc1[1], nc1[2], 0])
-Pc2nc2 = Matrix([nc2[0], nc2[1], nc2[2], 0])
-Pc1dc1 = Matrix([dc1[0], dc1[1], dc1[2], 0])
-Pc2dc2 = Matrix([dc2[0], dc2[1], dc2[2], 0])
 
 finalTw1 = dT1 * Tw1
 finalTw2 = dT2 * Tw2
 finalPw1 = finalTw1 * Pc1Matrix
 finalPw2 = finalTw2 * Pc2Matrix
-# finalRw1 = Matrix([finalTw1[0:3], finalTw1[4:7], finalTw1[8:11]])
-# finalRw2 = Matrix([finalTw2[0:3], finalTw2[4:7], finalTw2[8:11]])
-finalnw1 = finalTw1 * Pc1nc1
-finalnw2 = finalTw2 * Pc2nc2
-finaldw1 = finalTw1 * Pc1dc1
-finaldw2 = finalTw2 * Pc2dc2
 
-detPw = finalPw1 - finalPw2
-errn = detPw.transpose() * finalnw2
-errd = detPw.transpose() * finaldw2
+err = finalPw1 - finalPw2
 
 initValue = {r1:0, p1:0, w1:0, x1:0, y1:0, z1:0, r2:0, p2:0, w2:0, x2:0, y2:0, z2:0}
 with open(outputName+".txt", 'w') as f:
-    f.write("ValueType errn = " + str(errn[0].subs(initValue)) + ";\n")
-    f.write("ValueType errd = " + str(errd[0].subs(initValue)) + ";\n")
+    f.write("ValueType err0 = " + str(err[0].subs(initValue)) + ";\n")
+    f.write("ValueType err1 = " + str(err[1].subs(initValue)) + ";\n")
+    f.write("ValueType err2 = " + str(err[2].subs(initValue)) + ";\n")
     f.write("\n")
     def test2():
-        f.write(f"ValueType jac_n_T1_0 = " + str(diff(errn[0], r1).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_n_T1_1 = " + str(diff(errn[0], p1).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_n_T1_2 = " + str(diff(errn[0], w1).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_n_T1_3 = " + str(diff(errn[0], x1).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_n_T1_4 = " + str(diff(errn[0], y1).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_n_T1_5 = " + str(diff(errn[0], z1).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_n_T2_0 = " + str(diff(errn[0], r2).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_n_T2_1 = " + str(diff(errn[0], p2).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_n_T2_2 = " + str(diff(errn[0], w2).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_n_T2_3 = " + str(diff(errn[0], x2).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_n_T2_4 = " + str(diff(errn[0], y2).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_n_T2_5 = " + str(diff(errn[0], z2).subs(initValue)) + ";\n")
-
-        f.write(f"ValueType jac_d_T1_0 = " + str(diff(errd[0], r1).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_d_T1_1 = " + str(diff(errd[0], p1).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_d_T1_2 = " + str(diff(errd[0], w1).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_d_T1_3 = " + str(diff(errd[0], x1).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_d_T1_4 = " + str(diff(errd[0], y1).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_d_T1_5 = " + str(diff(errd[0], z1).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_d_T2_0 = " + str(diff(errd[0], r2).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_d_T2_1 = " + str(diff(errd[0], p2).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_d_T2_2 = " + str(diff(errd[0], w2).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_d_T2_3 = " + str(diff(errd[0], x2).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_d_T2_4 = " + str(diff(errd[0], y2).subs(initValue)) + ";\n")
-        f.write(f"ValueType jac_d_T2_5 = " + str(diff(errd[0], z2).subs(initValue)) + ";\n")
+        for i in range(3):
+            f.write(f"ValueType jac_e_" + str(i) + "_T1_0 = " + str(diff(err[i], r1).subs(initValue)) + ";\n")
+            f.write(f"ValueType jac_e_" + str(i) + "_T1_1 = " + str(diff(err[i], p1).subs(initValue)) + ";\n")
+            f.write(f"ValueType jac_e_" + str(i) + "_T1_2 = " + str(diff(err[i], w1).subs(initValue)) + ";\n")
+            f.write(f"ValueType jac_e_" + str(i) + "_T1_3 = " + str(diff(err[i], x1).subs(initValue)) + ";\n")
+            f.write(f"ValueType jac_e_" + str(i) + "_T1_4 = " + str(diff(err[i], y1).subs(initValue)) + ";\n")
+            f.write(f"ValueType jac_e_" + str(i) + "_T1_5 = " + str(diff(err[i], z1).subs(initValue)) + ";\n")
+            f.write(f"ValueType jac_e_" + str(i) + "_T2_0 = " + str(diff(err[i], r2).subs(initValue)) + ";\n")
+            f.write(f"ValueType jac_e_" + str(i) + "_T2_1 = " + str(diff(err[i], p2).subs(initValue)) + ";\n")
+            f.write(f"ValueType jac_e_" + str(i) + "_T2_2 = " + str(diff(err[i], w2).subs(initValue)) + ";\n")
+            f.write(f"ValueType jac_e_" + str(i) + "_T2_3 = " + str(diff(err[i], x2).subs(initValue)) + ";\n")
+            f.write(f"ValueType jac_e_" + str(i) + "_T2_4 = " + str(diff(err[i], y2).subs(initValue)) + ";\n")
+            f.write(f"ValueType jac_e_" + str(i) + "_T2_5 = " + str(diff(err[i], z2).subs(initValue)) + ";\n")
         f.write("\n")
     test2()
 
